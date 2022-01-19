aa8bd3599f51f9b2237441f10c7d53843decdce6
tgarayua <tgarayua@gmail.com>
2022-01-18 22:50:24 -0500

    Initial Commit

---
+++ Gaming News App/Gaming_News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gaming_News_AppApp.swift
+//  Gaming News App
+//
+//  Created by Thomas Garayua on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Gaming_News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
